448f2512de4b3f5e48d582dbfb00155684668a63
Pavan Kanajar <pkanajar@synapticon.com>
2013-11-20 13:41:34 +0100

    add license/comments

--- app_linux_fwupdate/src/fw_update.c
+++ app_linux_fwupdate/src/fw_update.c
@@ -1,5 +1,8 @@
-/** \file
- *  \Firmware Update for Synapticon SOMANET   -modified from Example code for Simple Open EtherCAT master
+/**
+ * \file fw_update.c
+ *
+ * \brief Firmware Update for Synapticon SOMANET (modified from Example code for
+ *   Simple Open EtherCAT master)
  *
  * Usage: fw_update eth0 OPTION id filepath 
  *    OPTION:
@@ -9,6 +12,35 @@
  *   -scan  scan the slave/slaves connected and display their serial number\n");
  *
  *   specify the number after options for these options -s & -seq followed //path//to//your//file
+ *
+ * SOEM is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * SOEM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * As a special exception, if other files instantiate templates or use macros
+ * or inline functions from this file, or you compile this file and link it
+ * with other works to produce a work based on this file, this file does not
+ * by itself cause the resulting work to be covered by the GNU General Public
+ * License. However the source code for this file must still be made available
+ * in accordance with section (3) of the GNU General Public License.
+ *
+ * This exception does not invalidate any other reasons why a work based on
+ * this file might be covered by the GNU General Public License.
+ *
+ * The EtherCAT Technology, the trade name and logo “EtherCAT” are the intellectual
+ * property of, and protected by Beckhoff Automation GmbH. You can use SOEM for
+ * the sole purpose of creating, using and/or selling or otherwise distributing
+ * an EtherCAT network master provided that an EtherCAT Master License is obtained
+ * from Beckhoff Automation GmbH.
+ *
+ * In case you did not receive a copy of the EtherCAT Master License along with
+ * SOEM write to Beckhoff Automation GmbH, Eiserstraße 5, D-33415 Verl, Germany
+ * (www.beckhoff.com).
  * 
  * Copyright 2013, Synapticon GmbH. All rights reserved
  * (c) Authors : Arthur Ketels 2010 - 2011 (original simpletest)
--- module_fwupdate/flash_somanet.h
+++ module_fwupdate/flash_somanet.h
@@ -1,21 +1,45 @@
 
 /**
- * \file flash_Somanet.h
  *
- * Copyright 2013, Synapticon GmbH. All rights reserved.
- * Authors: Pavan Kanajar <pkanajar@synapticon.com> & Frank Jeschke <jeschke@fjes.de>
+ * \file flash_somanet.h
  *
- * In the case where this code is a modification of existing code
- * under a separate license, the separate license terms are shown
- * below. The modifications to the code are still covered by the
- * copyright notice above.
+ * \brief Somanet Firmware Update implemtation
  *
- **/
+ *
+ * Copyright (c) 2013, Synapticon GmbH
+ * All rights reserved.
+ * Author: Frank Jeschke <jeschke@fjes.de> & Pavan Kanajar <pkanajar@synapticon.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Execution of this software or parts of it exclusively takes place on hardware
+ *    produced by Synapticon GmbH.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Synapticon GmbH Project.
+ *
+ */
 
 #ifndef FLASHSOMANET_H
 #define FLASHSOMANET_H
-#endif
-
 
 #include <stdint.h>
 #include <flash.h>
@@ -25,30 +49,37 @@
 
 #define BUFFER_SIZE 2400	//   flash requirement
 
-extern void flash_setup(int factory);
-extern int 	flash_buf_end(void);
-extern void chipReset(void);
-extern void flash_buffer(char content[], int imageSize, unsigned address);
+
 
 /**
- *  \brief
- *   Firmware update function (should be on CORE 0)
+ *  \brief Firmware update function
+ *    This thread must be on CORE 0 only
  *
  */
 extern void firmware_update(chanend foe_comm, chanend foe_signal, chanend reset);
 
 
+
+/* Internal Functions*/
+extern void flash_setup(int factory);
+
+extern int 	flash_buf_end(void);
+
+extern void chipReset(void);
+
+extern void flash_buffer(char content[], int imageSize, unsigned address);
+
 extern void get_file(chanend foe_out, char filename[]);
 
-
-/* If a file is available it is read by check_file_access() and the filesystem
+/*
+ * If a file is available it is read by check_file_access() and the filesystem
  * becomes formated for the next file.
  */
-
 int check_file_access(chanend foe_comm, unsigned address, chanend reset_out);
 
 /* software auto reset functions*/
 void reset_cores(chanend sig_in, chanend sig_out);
+
 void reset_last_core(chanend sig_in);
 
-/* FLASHSOMANET_H */
+#endif /* FLASHSOMANET_H */
